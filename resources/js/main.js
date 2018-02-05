74ae267e9aff585451303c9d6872a5fea67f8834
leonard <ballok.leonard@yahoo.com>
2018-02-05 22:07:22 +0000

    header animation added

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,14 +1,25 @@
 $(document).ready(function(){
   $('.carousel').slick({
     autoplay: true,
-    slidesToShow: 1,
+    slidesToSroll: 1,
     arrows: false,
     autoplaySpeed: 4000,
     draggable: false,
     pauseOnFocus: false,
     pauseOnHover: false
-
   });
 
   var headerScrollZero = 0;
+  $(window).scroll(function() {
+    var headerScrollCurrent = $(this).scrollTop();
+    if (headerScrollCurrent - headerScrollZero > 50) {
+      var navbarHeight = $(".nav").css("height");
+      $(".nav").animate({top: "-" + navbarHeight}, 150);
+      headerScrollZero = headerScrollCurrent;
+    }
+    else if(headerScrollZero - headerScrollCurrent > 50){
+      $(".nav").animate({top: "0px"}, 150);
+      headerScrollZero = headerScrollCurrent;
+    }
+  });
 });
