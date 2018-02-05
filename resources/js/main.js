d5ca1510401c9ca8736ba17ad967a31e998f3527
leonard <ballok.leonard@yahoo.com>
2018-02-05 16:27:55 +0000

    working carousel added

---
+++ resources/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    autoplay: true,
+    slidesToShow: 1,
+    arrows: false,
+    autoplaySpeed: 4000,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+
+  });
+
+  var headerScrollZero = 0;
+});
